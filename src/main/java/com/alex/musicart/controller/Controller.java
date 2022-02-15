95819cdbafc4b2ce33695dad65a0bf7fdea239cd
ashust75@gmail.com <ashust75@gmail.com>
2022-02-16 00:41:38 +0300

    Improvements and refactoring

--- src/main/java/com/alex/musicart/controller/Controller.java
+++ src/main/java/com/alex/musicart/controller/Controller.java
@@ -6,7 +6,6 @@ import com.alex.musicart.controller.command.ParameterName;
 import com.alex.musicart.exception.CommandException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
